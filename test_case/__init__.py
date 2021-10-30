0d9c0aab31f9e9ae094b62c88307e42d62067e3a
419758101 <419758101@qq.com>
2021-10-31 01:26:07 +0800

    request_16

--- test_case/__init__.py
+++ test_case/__init__.py
@@ -5,14 +5,14 @@ import datetime
 import json
 
 
-class DateEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime.datetime):
-            return obj.strftime("%Y-%m-%d")
-        else:
-            return json.JSONEncoder.default(self, obj)
-
-
-dic = {'name': 'jack', 'create_time': "2021-10-14"}
-print(json.dumps(dic))
-print(json.dumps(dic, cls=DateEncoder))
+# class DateEncoder(json.JSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, datetime.datetime):
+#             return obj.strftime("%Y-%m-%d")
+#         else:
+#             return json.JSONEncoder.default(self, obj)
+#
+#
+# dic = {'name': 'jack', 'create_time': "2021-10-14"}
+# print(json.dumps(dic))
+# print(json.dumps(dic, cls=DateEncoder))
